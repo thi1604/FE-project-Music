5970e00a9cd682402592318014a909d2f9a0e325
thi1604 <thibeo1604@gmail.com>
2025-03-07 12:12:34 +0700

    Sua lai base_url

--- src/app/components/global.tsx
+++ src/app/components/global.tsx
@@ -1,2 +1,2 @@
-export const base_url = "http://localhost:3000";
-// export const base_url = "https://music-project-y32u.onrender.com";
+// export const base_url = "http://localhost:3000";
+export const base_url = "https://music-project-y32u.onrender.com";
